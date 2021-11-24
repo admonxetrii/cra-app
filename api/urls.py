c9999446ab9ad44db49f72a8ce6a9446de6df917
Nisham Wagle <nishamwagle@gmail.com>
2021-11-24 15:06:51 +0545

    CRUD API created

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import RestaurantViewAPI
-
-# , RestaurantCreateAPI, RestaurantDetailAPI, RestaurantUpdateAPI, RestaurantDeleteAPI
+from .views import RestaurantViewAPI, CreateRestaurantAPI, DetailRestaurantAPI, UpdateRestaurantAPI, DeleteRestaurantAPI
 
 urlpatterns = [
     path('restaurants/', RestaurantViewAPI.as_view()),
-    # path('create-restaurant/', RestaurantCreateAPI.as_view()),
-    # path('get-restaurant/<int:id>', RestaurantDetailAPI.as_view()),
-    # path('update-restaurant/<int:id>', RestaurantUpdateAPI.as_view()),
-    # path('delete-restaurant/<int:id>', RestaurantDeleteAPI.as_view()),
+    path('create-restaurant/', CreateRestaurantAPI.as_view()),
+    path('get-restaurant/<int:id>', DetailRestaurantAPI.as_view()),
+    path('update-restaurant/<int:id>', UpdateRestaurantAPI.as_view()),
+    path('delete-restaurant/<int:id>', DeleteRestaurantAPI.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
 
 from .models import Restaurant
@@ -8,11 +9,57 @@ from .serializers import RestaurantSerializer
 # Create your views here.
 
 
-class RestaurantViewAPI(APIView):
+class RestaurantViewAPI(ListAPIView):
     permission_classes = []
     authentication_classes = []
+    # queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
 
-    def get(self, request, format=None):
+    def get_queryset(self):
         qs = Restaurant.objects.all()
-        serializer = RestaurantSerializer(qs, many=True)
-        return Response(serializer.data)
+        query = self.request.GET.get('q')
+        if query is not None:
+            qs = qs.filter(name__icontains=query)
+        return qs
+
+
+class CreateRestaurantAPI(CreateAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
+
+
+class DetailRestaurantAPI(RetrieveAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    # lookup_field = 'id'
+
+    def get_object(self, *args, **kwargs):
+        kwargs = self.kwargs
+        kw_id = kwargs.get('id')
+        return Restaurant.objects.get(id=kw_id)
+
+
+class UpdateRestaurantAPI(UpdateAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    lookup_field = 'id'
+
+
+
+class DeleteRestaurantAPI(DestroyAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    lookup_field = 'id'
+
+
